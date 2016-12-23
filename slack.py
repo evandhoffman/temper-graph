8603fa33c8712c026396987748b4284e70e72a6c
Evan Hoffman <evandhoffman@gmail.com>
2016-12-23 12:25:47 -0500

    Added slack.py

---
+++ slack.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+ 
+import slacker, time, sys, datetime, json
+
+
+# Tokens are generated at https://apps.twitter.com/
+with open('/root/temp-rrd/config.json') as cf:
+    config = json.load(cf)
+
+slack = slacker.Slacker(config['slack_api_key'])
+ 
+message = "Temperature as of %s" % '{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
+ 
+response = slack.files.upload("/home/evan/temperature_1h.png", title=message, channels='#eh_tempbot')
+
+print response
+

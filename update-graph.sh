46fce8f696d77b8a628fc1d3ba3f01fa31550b15
Evan <evandhoffman@gmail.com>
2016-12-23 09:11:25 -0500

    Trend lines

--- update-graph.sh
+++ update-graph.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-rrdtool graph /home/evan/temperature_graph.png \
--w 800 -h 600 -a PNG --slope-mode --start -1h --end now \
+
+
+for i in 1h 3h 6h 12h 24h 3d 7d ; do
+
+rrdtool graph /home/evan/temperature_$i.png \
+-w 800 -h 300 -a PNG --slope-mode --start -$i --end now \
 --title "Temperature" --watermark "`date`" --vertical-label "Temperature (F)" \
 DEF:Temperature=/home/evan/temp.rrd:Temperature:MAX \
-LINE1:Temperature#ff00ff:"Temp (F)"
+CDEF:avg=Temperature,1200,TRENDNAN \
+LINE1:Temperature#ff00ff:"Temp (F)" \
+LINE1:avg#00FF00:"Moving avg"
+
+done

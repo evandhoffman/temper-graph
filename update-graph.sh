1434c86dfc1927223c501a837dd352f827fbec58
Evan Hoffman <evandhoffman@gmail.com>
2017-01-03 11:37:42 -0500

    changed degrees to °

--- update-graph.sh
+++ update-graph.sh
@@ -10,7 +10,7 @@ for i in 1h 3h 6h 12h 24h 3d 7d ; do
 
 rrdtool graph /home/evan/temperature_$i.png \
 -w 400 -h 200 -a PNG --slope-mode --start -$i --end now \
---title "Temperature - $i - All Degrees Fahrenheit" --watermark "`date`" --vertical-label "Temperature (F)" \
+--title "Temperature - $i - All °F" --watermark "`date`" --vertical-label "Temperature (F)" \
 DEF:Temperature=/home/evan/temp.rrd:Temperature:MAX \
 CDEF:avg=Temperature,1800,TRENDNAN \
 VDEF:last=Temperature,LAST \
@@ -18,19 +18,19 @@ VDEF:max=Temperature,MAXIMUM \
 VDEF:min=Temperature,MINIMUM \
 VDEF:avg1=Temperature,AVERAGE \
 AREA:Temperature$COLOR_TEMP:"Temp" \
-GPRINT:last:"%6.2lf%sDegrees\\n" \
+GPRINT:last:"%6.2lf%s°F\\n" \
 LINE1:Temperature#0000ffcc:"" \
 COMMENT:"\\n" \
 HRULE:avg1#cccc00cc:"Avg" \
-GPRINT:avg1:"%6.2lf%sDegrees" \
+GPRINT:avg1:"%6.2lf%s°F" \
 COMMENT:"\\n" \
 \
 HRULE:min$COLOR_MIN:"Min" \
-GPRINT:min:"%6.2lf%sDegrees" \
+GPRINT:min:"%6.2lf%s°F" \
 COMMENT:"\\n" \
 \
 HRULE:max$COLOR_MAX:"Max" \
-GPRINT:max:"%6.2lf%sDegrees" \
+GPRINT:max:"%6.2lf%s°F" \
 COMMENT:"\\n" \
 LINE2:avg$COLOR_MOVING_AVG:"30-minute Average\\n" 
 

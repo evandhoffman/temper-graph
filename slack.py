692b20126655895eb3904bcd54555f239d1117b7
Evan Hoffman <evandhoffman@gmail.com>
2017-02-23 16:16:50 -0500

    More elaborate slack messages.

--- slack.py
+++ slack.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
  
-import slacker, time, sys, datetime, json, ntpath
+import slacker, time, sys, datetime, json, ntpath, os.path, commands
 
 
 # Tokens are generated at https://apps.twitter.com/
@@ -12,9 +12,35 @@ slack = slacker.Slacker(config['slack_api_key'])
 
 filename = sys.argv[1]
  
-message = "Temperature as of %s \n %s" % ('{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()), ntpath.basename(filename))
+if (os.path.isfile(filename)):
+	message = "Temperature as of %s \n %s" % ('{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()), ntpath.basename(filename))
  
-response = slack.files.upload(filename, title=message, channels='#eh_tempbot')
+	response = slack.files.upload(filename, title=message, channels='#eh_tempbot')
 
-print response
+	print response
+else:
+	RRD_PATH="/home/evan/"
+
+	cmd = "rrdtool lastupdate %s/temp.rrd | tail -1" % RRD_PATH
+	rrdtemp = commands.getstatusoutput(cmd)
+
+	t = rrdtemp[1].split(': ')
+	temp = "%s °F" % (t[1])
+
+	if (t[1] < 73):
+		color = "good"
+		emoji = ":thumbsup:"
+	elif (t[1] < 77):
+		color = "warning"
+		emoji = ":beach_with_umbrella:"
+	else: 
+		color = "danger"
+		emoji = ":skull_and_crossbones:"
+
+	a = { "fallback":temp, "color": color, "text":temp+ " " + emoji }
+	
+
+	slack.chat.post_message(channel='#eh_tempbot', username='TempBot', attachments=[a], text="")
+
+
 
--- twitter-txt.py
+++ twitter-txt.py
@@ -4,7 +4,7 @@
 import tweepy, time, sys, datetime, json, commands
 
 
-RRD_PATH="/root/temper-graph/"
+RRD_PATH="/root/temp-rrd/"
 
 # Tokens are generated at https://apps.twitter.com/
 with open("%s/config.json" % RRD_PATH ) as cf:
@@ -14,6 +14,8 @@ with open("%s/config.json" % RRD_PATH ) as cf:
 auth = tweepy.OAuthHandler(config['consumer_key'], config['consumer_secret'])
 auth.set_access_token(config['access_key'], config['access_secret'])
 api = tweepy.API(auth)
+
+RRD_PATH="/home/evan/"
 
 cmd = "rrdtool lastupdate %s/temp.rrd | tail -1" % RRD_PATH
 rrdtemp = commands.getstatusoutput(cmd)

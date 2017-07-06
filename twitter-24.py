b11db3423bcc5d71e3566ac3be5725f05a91b389
Evan Hoffman <evandhoffman@gmail.com>
2017-07-06 09:47:04 -0400

    added

---
+++ twitter-24.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+ 
+import tweepy, time, sys, datetime, json
+
+
+# Tokens are generated at https://apps.twitter.com/
+with open('/root/temp-rrd/config.json') as cf:
+    config = json.load(cf)
+ 
+#enter the corresponding information from your Twitter application:
+auth = tweepy.OAuthHandler(config['consumer_key'], config['consumer_secret'])
+auth.set_access_token(config['access_key'], config['access_secret'])
+api = tweepy.API(auth)
+
+message = "Temperature as of %s" % '{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
+ 
+api.update_with_media("/home/evan/temperature_24h.png", message)
+
